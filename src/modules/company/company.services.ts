12d4a162200b9d9e112fcab82eb502f64fc25ba7
Samuel Custódio <samuel.rodrigues@aluno.unievangelica.edu.br>
2024-11-10 16:26:27 -0300

    #fix:company.service

--- src/modules/company/company.services.ts
+++ src/modules/company/company.services.ts
@@ -4,7 +4,5 @@ import { CreateCompanyInput } from "./company.schemas";
 export async function createCompany(input: CreateCompanyInput) {
   const { name, works } = input;
 
-  const company = await prisma.company.create({
-    data: { name, works: { create: works.map((work) => ({ name: work })) } },
-  });
+  console.log("works", works);
 }
